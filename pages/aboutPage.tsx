e935c6d0f14ab175537e51ecc2aba1f4fd9ebde5
edmondywen <edmondywen@gmail.com>
2022-08-17 21:40:01 -0700

    work on about page

--- pages/aboutPage.tsx
+++ pages/aboutPage.tsx
@@ -7,7 +7,20 @@ export default function AboutPage() {
   return (
     <MainLayout>
       <div className={styles.main}>
-        <h1 className={styles.description}>Here&apos;s the about page</h1>
+        <div className={styles.content}>
+          <h1 className={styles.description}>Background</h1>
+          <p> You are more than your resume. The goal of Anti-Resume is to
+            provide a platform that showcases the failures and accomplishments  </p>
+          {/* of Bruins from all backgrounds. We at ACM aim to normalize discussions of
+          failure and imposter syndrome and promote a campus community where
+          everyone can thrive. I GOT LINTED WHATS*/}
+        </div>
+
+        <h1 className={styles.description}>How can I contribute my Anti-Resume?</h1>
+        <a href="https://forms.gle/jGuKbj1pgnXWq3T77" target="_blank" rel="noopener noreferrer">
+          <button>Add your anti-resume</button>
+        </a>
+        <h1 className={styles.description}>Reach out!</h1>
         <Link href="/">
           <button>Go back home</button>
         </Link>
