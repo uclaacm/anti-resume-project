d57a9a1ee6abea5b6b99d2b5492f5f19a25d8aaf
Amy He <amyh11325@gmail.com>
2022-09-04 16:02:44 -0700

    Replace API key with service account for google api

--- util/sheets.ts
+++ util/sheets.ts
@@ -3,33 +3,46 @@ import { google } from 'googleapis';
 import { Resume } from './types';
 
 dotenv.config();
-const SHEETS_API_KEY = process.env.SHEETS_API_KEY;
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+const SERVICE_ACCOUNT = process.env.SERVICE_ACCOUNT ?? '';
 
 async function getPeople(year: number): Promise<Resume[]> {
   const sheets = google.sheets('v4');
 
-    // Get roles from google sheets
-    const res = await sheets.spreadsheets.values.get(
-      {
-        spreadsheetId: SPREADSHEET_ID,
-        range: `${year}!A2:F`,
-        key: SHEETS_API_KEY,
-      });
-    const output = [];
-    const rows = res?.data.values ?? [];
-    for (const row of rows) {
-      output.push({
-        user: row[1],
-            year: row[3],
-            dateModified: row[0],
-            toInforms: row[2].split('\n') ?? null,
-            iHave: row[5].split('\n') ?? null,
-            image: row[4],
-          });
-        }
+  // Get JWT Token to access sheet
+  const service_account = JSON.parse(SERVICE_ACCOUNT);
+  const jwtClient = new google.auth.JWT(
+    service_account.client_email,
+    '',
+    service_account.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets'],
+  );
+  jwtClient.authorize(function (err) {
+    if (err) {
+      throw err;
+    }
+  });
 
-    return output;
+  // Get roles from google sheets
+  const res = await sheets.spreadsheets.values.get({
+    auth: jwtClient,
+    spreadsheetId: SPREADSHEET_ID,
+    range: `${year}!A2:F`,
+  });
+  const output = [];
+  const rows = res?.data.values ?? [];
+  for (const row of rows) {
+    output.push({
+      user: row[1],
+      year: row[3],
+      dateModified: row[0],
+      toInforms: row[2].split('\n') ?? null,
+      iHave: row[5].split('\n') ?? null,
+      image: row[4],
+    });
+  }
+
+  return output;
 }
 
 export default getPeople;
